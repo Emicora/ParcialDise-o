49511e2c8742d77b4dec436792b556e92f161003
Emicora <e.coratolo@alumno.um.edu.ar>
2024-11-04 12:56:07 -0300

    render2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers.mutant_controller import mutant_bp
 from database.db_connection import init_db
@@ -11,4 +12,5 @@ init_db()
 app.register_blueprint(mutant_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
